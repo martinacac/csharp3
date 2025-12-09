8950ac5a6aec0f46ddacd3af419a96e81e10c536
Martina Cachová <martinacachova@seznam.cz>
2025-12-09 20:36:09 +0100

    Lekce 11

--- ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
+++ ToDoList/src/ToDoList.Frontend/Models/ToDoItemView.cs
@@ -1,10 +1,14 @@
 namespace ToDoList.Frontend.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 //public record ToDoItemView(int Id, string Name, string Description, bool IsCompleted);
 public class ToDoItemView
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Name is mandatory.")]
     public string Name { get; set; }
+    [StringLength(250)]
     public string Description { get; set; }
     public bool IsCompleted { get; set; }
     public string? Category { get; set; }
